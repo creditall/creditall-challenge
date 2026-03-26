08d4cf7ff0661754c49646d546db4e428e2bdbf5
Matheus Pereira <matheus123.mp45@gmail.com>
2026-03-26 14:35:59 -0300

    feat: config inicial da API e conexao com banco

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from app.core.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "API ON"}
