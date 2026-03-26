080f6a49cd9ddc12f79d3c853fd5ffb926684778
Matheus Pereira <matheus123.mp45@gmail.com>
2026-03-26 15:10:18 -0300

    feat: adiciona validacao de email em clientes

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
 
-from app.core.database import Base, engine
+from app.api.routes.clients import router as clients_router
 from app.api.routes.products import router as products_router
+from app.core.database import Base, engine
+from app.models.client import Client
+from app.models.product import Product
+from app.models.sale import Sale
 
 # cria as tabelas no banco
 Base.metadata.create_all(bind=engine)
 
-# instancia a aplicação FastAPI
+# instancia a aplicação
 app = FastAPI()
 
-# registra as rotas de produtos
+# registra as rotas
 app.include_router(products_router)
+app.include_router(clients_router)
+
 
 @app.get("/")
 def root():
